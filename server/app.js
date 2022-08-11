eef012c4460bd83c0455f92eec4f08c49b75a5d0
aemartos <aemartos@gmail.com>
2022-08-11 15:06:21 +0200

    refactor: reorder server files

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ mongoose.connect(process.env.DBURL, {useNewUrlParser: true})
   .then(x => {console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);})
   .catch(err => {console.error('Error connecting to mongo', err);});
 
-const app_name = require('../package.json').name;
+const app_name = require('./package.json').name;
 require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
 
 const app = express();
